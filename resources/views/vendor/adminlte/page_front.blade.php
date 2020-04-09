6e83890503e86346dbd07fa9ade9075c9c91d2b5
yuan8 <bhalayuan@gmail.com>
2020-04-09 16:21:00 +0700

    ok

--- resources/views/vendor/adminlte/page_front.blade.php
+++ resources/views/vendor/adminlte/page_front.blade.php
@@ -92,17 +92,19 @@
 
         <!-- Content Wrapper. Contains page content -->
         <div class="content-wrapper">
+              <section class="content-header">
+                 <div class="row">
+                 </div>
+                  @yield('content_header')
+                 
+                
+            </section>
+
             @if(config('ltefron.layout') == 'top-nav')
             <div class="container-fluid">
             @endif
 
             <!-- Content Header (Page header) -->
-            <section class="content-header">
-                 <div class="row">
-                  
-                 </div>
-                @yield('content_header')
-            </section>
 
             <!-- Main content -->
             <section class="content" style="min-height: 100vh">
