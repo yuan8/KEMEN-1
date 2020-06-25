ba5eb508fa650274953e09bf99385cf49cdb1a0c
yuan8 <bhalayuan@gmail.com>
2020-06-26 00:41:31 +0700

    xs

--- app/NodeJS/SIPD/index.js
+++ app/NodeJS/SIPD/index.js
@@ -27,7 +27,7 @@ async function login(){
 	try{
 
 
-	const browser =  await puppeteer.launch({headless: false,ignoreHTTPSErrors: true,defaultViewport:null,args,
+	const browser =  await puppeteer.launch({headless: false,ignoreHTTPSErrors: true,defaultViewport:null,
       args: ['--no-sandbox']
   });
 
